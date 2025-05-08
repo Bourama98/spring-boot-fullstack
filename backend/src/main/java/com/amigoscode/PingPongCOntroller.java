1124e0b662bef6cb8d9225ce620ebad74346a36c
Development_PC_main <mangara98@hotmail.com>
2025-05-08 00:21:53 -0700

    Updated email

--- backend/src/main/java/com/amigoscode/PingPongCOntroller.java
+++ backend/src/main/java/com/amigoscode/PingPongCOntroller.java
@@ -9,6 +9,6 @@ public class PingPongCOntroller {
 
     @GetMapping("/ping")
     public PingPong getPingPong(){
-        return new PingPong("pong: Mangara Bourama 223");
+        return new PingPong("pong: Mangara Bourama 2237778");
     }
 }
