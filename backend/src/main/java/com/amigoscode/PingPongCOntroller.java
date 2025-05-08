f27405a3cef06510738c4fa93ff693d2b77d22fd
Development_PC_main <mangara98@hotmail.com>
2025-05-08 00:59:59 -0700

    JIB and AWS44

--- backend/src/main/java/com/amigoscode/PingPongCOntroller.java
+++ backend/src/main/java/com/amigoscode/PingPongCOntroller.java
@@ -9,6 +9,6 @@ public class PingPongCOntroller {
 
     @GetMapping("/ping")
     public PingPong getPingPong(){
-        return new PingPong("pong: Mangara 778899");
+        return new PingPong("pong: Mangara 77889944");
     }
 }
