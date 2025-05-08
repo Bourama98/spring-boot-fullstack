6d17429f422d81ec5912b05caa559d28cd30bd2c
Development_PC_main <mangara98@hotmail.com>
2025-05-08 01:17:38 -0700

    Refactor the pom.xml file and rtemove hard code secrets

--- backend/src/main/java/com/amigoscode/PingPongController.java
+++ backend/src/main/java/com/amigoscode/PingPongController.java
@@ -4,11 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PingPongCOntroller {
-    record PingPong(String result){}
+public class PingPongController {
+    record PingPong(String result) {
+    }
 
     @GetMapping("/ping")
-    private PingPong getPingPong(){
+    private PingPong getPingPong() {
         return new PingPong("pong: Mangara AWA77");
     }
 }
