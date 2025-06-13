1e56dfb9770247b58084880291a21235c004fb10
Development_PC_main <mangara98@hotmail.com>
2025-06-12 23:47:16 -0700

    CD Depoy frontend branch with update and delete customer feature

--- backend/src/main/java/com/amigoscode/PingPongController.java
+++ backend/src/main/java/com/amigoscode/PingPongController.java
@@ -10,6 +10,6 @@ public class PingPongController {
 
     @GetMapping("/ping")
     private PingPong getPingPong() {
-        return new PingPong("pong: Ping 8");
+        return new PingPong("pong: Ping 8888");
     }
 }
